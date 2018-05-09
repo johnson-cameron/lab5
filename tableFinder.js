d2e4c83408f8f1de4c13a01615a489de32a6aeef
cameron <johnson.cameron.m@gmail.com>
2018-05-09 07:04:16 -0400

    jquery update

--- tableFinder.js
+++ tableFinder.js
@@ -2,22 +2,43 @@
 
 $(document).ready(() => {
 // do everything in this
-let table = $(".available");
-let button = $("#submitRes");
-let exit = $("img");
+    let table = $(".available");
+    let button = $("#submitRes");
+    let exit = $("img");
+    
 
-table.click(() => {
-    $(".form").toggle()
-});
+    $(document).on("click", ".available", (event) => {
+        $(".form").fadeIn(1000)
+        $("#tabnum").text(`Table Number: ${$(event.target).text()}`)
+        $("#name").val("")
+        $("#phone").val("")
+        $("#party").val("")
+        exit.click(() => {
+            $(".form").hide()
+            event = "";
+        });
+        
+        // when the button is clicked the clicked element adds the reserved class and removes the available class
+        // then 
+        button.click(() => {
+            $(".form").hide()
+            $(event.target).addClass("reserved").removeClass("available")
+            $(event.target).attr("name", $("#name").val());
+            $(event.target).attr("phone", $("#phone").val());
+            $(event.target).attr("size", $("#party").val());
+            event= "";
+        });
+    })
+    .on("mouseleave", ".reserved", (event) => {
+        console.log("it left")
+        $(".info").html("");
+        $(".info").hide();
 
-button.click(() => {
-    $(".form").toggle()
-    $(".table").addClass("reserved")
-    $(".table").removeClass("available")
-});
-
-exit.click(() => {
-    $(".form").toggle()
-});
-
+    })
+    .on("mouseenter", ".reserved", (event) => {
+        let table = $(event.target).contents();
+        $(".info").append(`<p>Name: ${$(event.target).attr("name")}</p><p>Party Size: ${$(event.target).attr("size")}</p>`);
+        table.fadeIn(1000);
+    });
+    
 });
